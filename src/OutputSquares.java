178f7a342c5049de10190ea8cf8882b1e1ff8c3f
Samarth Rao <raosamarth91@gmail.com>
2020-09-22 23:07:10 -0700

    Output squares added

---
+++ src/OutputSquares.java
@@ -0,0 +1,18 @@
+public class OutputSquares {
+    public static void main(String[] args) {
+        int givenNumber = 100;
+      int difference =1;
+      int square;
+
+   for(int i =1; i <= givenNumber;i = square) {
+
+        difference = difference +2;
+      square =  i + difference;
+      System.out.println(square);
+
+   }
+
+
+   }
+    }
+
