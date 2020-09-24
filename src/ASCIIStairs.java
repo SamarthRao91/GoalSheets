2a5bdaa41b9b1f7021b35046db788247cdb4daa7
Samarth Rao <raosamarth91@gmail.com>
2020-09-24 00:09:13 -0700

    ASCII Stairs(Not Finished Yet)

---
+++ src/ASCIIStairs.java
@@ -0,0 +1,10 @@
+public class ASCIIStairs {
+    public static void main(String[] args) {
+
+        int numberOfStairs = 5;
+        for(int i = 1; i <= numberOfStairs; i++)
+        {
+            System.out.println("j");
+        }
+    }
+}
