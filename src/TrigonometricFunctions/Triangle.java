e24c15f8a5a79106af7db956c51f9b7f047279cf
Samarth Rao <raosamarth91@gmail.com>
2020-09-24 09:45:38 -0700

    Made side lengths and angles doubles

--- src/TrigonometricFunctions/Triangle.java
+++ src/TrigonometricFunctions/Triangle.java
@@ -2,19 +2,19 @@ package TrigonometricFunctions;
 public class Triangle {
     public static void main(String[] args) {
 
-        triangle(9,7,3);
+        triangle(1,1.414,1);
 
     }
 
-    public static void triangle(int side1, int side2, int side3)
+    public static void triangle(double side1, double side2, double side3)
     {
         //Calculating first Angle
-       double CosineA = ((double)(side2 *side2) + (double)(side3 * side3)  - (double)(side1 * side1))/(2*side2*side3);
+       double CosineA = ((side2 *side2) + (double)(side3 * side3)  - (double)(side1 * side1))/(2*side2*side3);
 
          double ArcSineA = Math.acos(CosineA);
          ArcSineA =ArcSineA * (180/3.14);
 
-        int Angle1 = (int)ArcSineA;
+        double Angle1 = ArcSineA;
         System.out.println("First Angle: " + Angle1 + "\n");
         //End of Calculating first Angle
 
@@ -23,12 +23,12 @@ public class Triangle {
 
         double ArcSineB = Math.acos(CosineB);
         ArcSineB =ArcSineB * (180/3.14);
-        int Angle2 = (int)ArcSineB;
+        double Angle2 = ArcSineB;
         System.out.println("Second Angle: " + Angle2 + "\n");
         //end of calculation second angle
 
         // Calculate 3rd angle
-        int Angle3 = 180 - (Angle1 + Angle2);
+        double Angle3 = 180 - (Angle1 + Angle2);
         System.out.println("Third Angle: " + Angle3);
         //end of calculating third angle
 
