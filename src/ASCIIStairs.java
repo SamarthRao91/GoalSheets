55cfbb44efcd685ddb0a4589fcefd0d3401ac7dd
Samarth Rao <raosamarth91@gmail.com>
2020-09-25 21:56:36 -0700

    ASCII Stairs(In progress)

--- src/ASCIIStairs.java
+++ src/ASCIIStairs.java
@@ -1,10 +1,25 @@
 public class ASCIIStairs {
     public static void main(String[] args) {
-
         int numberOfStairs = 5;
-        for(int i = 1; i <= numberOfStairs; i++)
-        {
-            System.out.println("j");
+
+
+        for (int i = numberOfStairs; i >= 0; i--) {
+            //printSpaces(i);
+            System.out.print("   o    *");
+            System.out.print("*****  \n");
+            printSpaces(i);
+            System.out.print("       /|\\    *\n" );
+            printSpaces(i);
+            System.out.print("       / \\    *\n");
+            printSpaces(i);
         }
     }
+    public static void printSpaces(int ammount)
+    {
+        for(int d = 0; d <= ammount; d++ ) {
+            System.out.print("     ");
+        }
+    }
+
 }
+
