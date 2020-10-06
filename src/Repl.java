5937108b95d44ffcef39a0f599644bde93ba5c35
Samarth Rao <raosamarth91@gmail.com>
2020-10-06 13:15:09 -0700

    GS03-1

---
+++ src/Repl.java
@@ -0,0 +1,22 @@
+    public class Repl {
+
+        public static void main(String[] args) {
+            String result;
+
+
+            result = repl("hello",10);
+            System.out.println(result);
+        }
+     public static String repl(String value, int repetitions )
+     {  String start = "";
+         for(int i = 0; i < repetitions; i++)
+         {
+
+            start = start + value;
+
+         }
+
+         return start;
+     }
+
+    }
