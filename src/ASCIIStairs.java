33f82603929e4a5d54296586bdca3c1886d11fcb
Samarth Rao <raosamarth91@gmail.com>
2020-10-12 21:48:13 -0700

    Update ASCIIStairs.java

--- src/ASCIIStairs.java
+++ src/ASCIIStairs.java
@@ -1,11 +1,7 @@
 public class ASCIIStairs {
-
-
-
     public static void main(String[] args) {
         int numberOfStairs = 0;
          String bottomBar = "                 ****";
-
 
         for (int i = numberOfStairs; i >= 0; i--) {
             printSpaces(i);
@@ -15,16 +11,13 @@ public class ASCIIStairs {
             System.out.print("            /|\\ *\n");
             printSpaces(i);
             System.out.print("            / \\ *\n");
-          //  System.out.println("                                                    *");
-           // System.out.println("                                                    *");
-            //System.out.println("                                                    ");
+
         }
-        for (int i = numberOfStairs; i >= 0; i--) {
+        for (int i = numberOfStairs; i >= 1; i--) {
             bottomBar = bottomBar + "******";
         }
         System.out.println(bottomBar);
     }
-
     public static void printSpaces(int ammount)
     {       
         for(int d = 0; d <= ammount; d++ ) {
