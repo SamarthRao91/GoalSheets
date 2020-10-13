a9bc856d2540f630ca3ca2ef097e829f9ac7d18b
Samarth Rao <raosamarth91@gmail.com>
2020-10-12 17:13:33 -0700

    Added the bottom bar to ASCII stair

--- src/ASCIIStairs.java
+++ src/ASCIIStairs.java
@@ -4,13 +4,7 @@ public class ASCIIStairs {
 
     public static void main(String[] args) {
         int numberOfStairs = 0;
-
-
-
-
-        
-
-
+         String bottomBar = "                 ****";
 
 
         for (int i = numberOfStairs; i >= 0; i--) {
@@ -18,15 +12,19 @@ public class ASCIIStairs {
             System.out.print("             o");
             System.out.print("   *****  \n");
             printSpaces(i);
-            System.out.print("            /|\\ *\n" );
+            System.out.print("            /|\\ *\n");
             printSpaces(i);
             System.out.print("            / \\ *\n");
-            System.out.println("                                                    *");
-
+          //  System.out.println("                                                    *");
+           // System.out.println("                                                    *");
+            //System.out.println("                                                    ");
         }
-
-       // System.out.println("                     ********************************");
+        for (int i = numberOfStairs; i >= 0; i--) {
+            bottomBar = bottomBar + "******";
+        }
+        System.out.println(bottomBar);
     }
+
     public static void printSpaces(int ammount)
     {       
         for(int d = 0; d <= ammount; d++ ) {
