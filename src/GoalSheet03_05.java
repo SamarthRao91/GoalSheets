5c390c9e65034f91fe8074b79b886346d1daad6d
Samarth Rao <raosamarth91@gmail.com>
2020-10-17 19:49:33 -0700

    Finished charCounter problem

--- src/GoalSheet03_05.java
+++ src/GoalSheet03_05.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
 public class GoalSheet03_05 {
+    public static void main(String[] args) {
+        int wordCount = 0;
+        int emptySpace = 32;
+        char charEmptySpace = (char) (emptySpace);
+      Scanner charInput = new Scanner(System.in);
+      System.out.println("Please input the values");
+      String paperInput = charInput.nextLine();
+
+           for(int c =0; c < paperInput.length(); c++)
+           {
+               if(paperInput.charAt(c) == charEmptySpace)
+               {
+                   wordCount++;
+               }
+           }
+        int charCount = paperInput.length() - wordCount;
+
+      System.out.print("WordCount: ");
+       System.out.println(wordCount + 1);
+        System.out.println("characterCount: " + charCount);
+
+
+    }
+
+
 }
