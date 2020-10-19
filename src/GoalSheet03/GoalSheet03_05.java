d2a5a958ba95230b9ef47fac6d21399a3b77f018
Samarth Rao <raosamarth91@gmail.com>
2020-10-19 13:10:33 -0700

    Fixed char and word counter program

--- src/GoalSheet03/GoalSheet03_05.java
+++ src/GoalSheet03/GoalSheet03_05.java
@@ -1,53 +1,39 @@
 package GoalSheet03;
 
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 public class GoalSheet03_05 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+
+        Scanner fileInput = new Scanner(new File("C:\\Users\\raosa\\IdeaProjects\\GoalSheets\\src\\GoalSheet03\\doc.txt"));
         int wordCount = 0;
-        int emptySpace = 32;
-        int endOfLine = 0x0D0A0;
-        char charEmptySpace = (char) (emptySpace);
-      Scanner charInput = new Scanner(System.in);
-      System.out.println("Please input the values");
-      String paperInput = charInput.nextLine();
+        int characterCounter = 0;
+        while (fileInput.hasNext()) {
+            wordCount++;
+            String word = fileInput.next();
 
-           for(int c =0; c < paperInput.length(); c++)
-           {
-               if(paperInput.charAt(c) == charEmptySpace||paperInput.charAt(c) == endOfLine)
-               {
-                   wordCount++;
-               }
-           }
-        int charCount = paperInput.length() - wordCount;
+            for (int i = 33; i <= 126; i++) {
+                char iNumber = (char)(i);
+                for (int c = 0; c < word.length(); c++) {
+                    if (word.charAt(c) == iNumber) {
 
-      System.out.print("WordCount: ");
-       System.out.println(wordCount + 1);
-        System.out.println("characterCount: " + charCount);
+                     characterCounter++;
+                    }
+                }
 
 
-    }
-
-
-    public static class Repl {
-
-        public static void main(String[] args) {
-            String result;
-
-
-            result = repl("hello",10);
-            System.out.println(result);
+            }
         }
-     public static String repl(String value, int repetitions )
-     {  String start = "";
-         for(int i = 0; i < repetitions; i++)
-         {
-
-            start = start + value;
-
-         }
-
-         return start;
-     }
-
+        System.out.println("WordCounter: " + wordCount);
+        System.out.println("characterCounter: " + characterCounter);
     }
 }
+
+
+
+
+
+
+
+
