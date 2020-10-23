e96f613d7c129c9b22d32bd09701d687bd8e22f2
Samarth Rao <raosamarth91@gmail.com>
2020-10-22 18:00:09 -0700

    Finished processName Assignment

--- src/GoalSheet04/processName_03.java
+++ src/GoalSheet04/processName_03.java
@@ -3,6 +3,16 @@ package GoalSheet04;
 public class processName_03 {
     public static void main(String[] args)
     {
+      processesName("Hi my name is Samarth");
 
+    }
+    public static void processesName(String input)
+    {
+        String[] nameArray = input.split("\\s");
+        int arrayCounter = nameArray.length;
+        for(int i =nameArray.length; i >= 1; i--)
+        {
+            System.out.print(nameArray[i-1] + " ");
+        }
     }
 }
