58e1f86b0b8aaa1c8acec3512bb45cd9c40fc55b
Samarth Rao <raosamarth91@gmail.com>
2020-10-22 17:26:19 -0700

    Sorted GS and made some changes to chasr count assignment

--- src/GoalSheet03/GoalSheet03_05.java
+++ src/GoalSheet03/GoalSheet03_05.java
@@ -9,24 +9,36 @@ public class GoalSheet03_05 {
         Scanner fileInput = new Scanner(new File("C:\\Users\\raosa\\IdeaProjects\\GoalSheets\\src\\GoalSheet03\\doc.txt"));
         int wordCount = 0;
         int characterCounter = 0;
-        while (fileInput.hasNext()) {
+        int countOfLines=0;
+
+
+
+        while (fileInput.hasNextLine()) {
+
+
             wordCount++;
             String word = fileInput.next();
 
-            for (int i = 33; i <= 126; i++) {
+            for (int i = 33; i <= 126; i++ ) {
                 char iNumber = (char)(i);
                 for (int c = 0; c < word.length(); c++) {
                     if (word.charAt(c) == iNumber) {
 
                      characterCounter++;
                     }
+                    if( word.charAt(c) == '\n')
+                    {
+                        countOfLines++;
+                    }
                 }
 
 
             }
+
         }
         System.out.println("WordCounter: " + wordCount);
         System.out.println("characterCounter: " + characterCounter);
+        System.out.println("lineCount: " + countOfLines);
     }
 }
 
