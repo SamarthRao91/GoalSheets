4c78a79b0daf62b56830d01f561725f3a60076be
Samarth Rao <raosamarth91@gmail.com>
2020-10-27 19:15:14 -0700

    Fixed print range

--- src/GoalSheet03/PrintRange.java
+++ src/GoalSheet03/PrintRange.java
@@ -2,7 +2,7 @@ package GoalSheet03;
 
 public class PrintRange {
     public static void main(String[] args) {
-       printRange(10000, 1);
+       printRange(1, 100);
     }
     public static void printRange(int start, int end)
     {
@@ -13,11 +13,15 @@ public class PrintRange {
                 System.out.print(i+ " ,");
             }
             System.out.print("}");
+
         }
+
         else if(start == end)
-        {
+        {  System.out.print("{");
             System.out.println(start);
+            System.out.print("}");
         }
+
         else {
             System.out.print("{");
             for (int i = start; i <=end; i++) {
