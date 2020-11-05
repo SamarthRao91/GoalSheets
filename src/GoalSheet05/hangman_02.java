6aae73af42c117cda9d0fbcf3d5e709b1b98f177
Samarth Rao <raosamarth91@gmail.com>
2020-11-04 20:15:45 -0800

    Finished hangman problem

--- src/GoalSheet05/hangman_02.java
+++ src/GoalSheet05/hangman_02.java
@@ -10,10 +10,9 @@ public class hangman_02 {
         String word = "cool";
         boolean wordCorrect = false;
         String[] wordArray = new String[word.length()];
-        String[] userWord = new String[word.length()];
+        int counter =0;
         wordArray = word.split("(?!^)");
-        int counter = 0;
-        while (gameFinished == false) {
+        while (!gameFinished) {
             //Getting the input from the user
             Scanner userInput = new Scanner(System.in);
             System.out.println("Please enter a letter");
@@ -21,29 +20,28 @@ public class hangman_02 {
 
             //check the user input
             for (int i = wordArray.length - 1; i >= 0; i--) {
-                if (input == wordArray[i]) {
+                if (input.equals(wordArray[i])) {
 
                     wordCorrect = true;
-                }
 
-                if(wordCorrect == true)
-                {
-                    System.out.println("That letter has correct");
-                    input = userWord[i];
-                    wordCorrect = false;
                 }
-                else
-                {
-                    System.out.println("The letter you guessed was incorrect");
-                }
-
             }
 
+            if(wordCorrect)
+            {
+                System.out.println("That letter has correct");
+                counter++;
+                wordCorrect = false;
+            }
+            else
+            {
+                System.out.println("The letter you guessed was incorrect");
+            }
 
             //check if the user has finished the game
             if (counter == word.length()) {
                 gameFinished = true;
-                System.out.println("The Game has finished, you have won");
+                System.out.println("You have guessed all the letters in the word. The word was: " + word);
             }
         }
        }
