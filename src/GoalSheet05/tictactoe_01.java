27487e36fc1a7777baf27a564b74eaa5054fe36c
Samarth Rao <raosamarth91@gmail.com>
2020-11-06 19:20:14 -0800

    made some changes to tic tac toe game

--- src/GoalSheet05/tictactoe_01.java
+++ src/GoalSheet05/tictactoe_01.java
@@ -1,5 +1,4 @@
 package GoalSheet05;
-import java.io.*;
 import java.util.*;
 public class tictactoe_01 {
     public static void main(String[] args) {
@@ -7,58 +6,61 @@ public class tictactoe_01 {
         boolean gameWon = false;
         String turn = "X";
 
-        String X = "X";
-        String O = "O";
+
 
        String board[] = new String[9];
 
        while(!gameWon)
        {
            System.out.println("Select a tile to place on by entering a value 1 to 9 (top left of the board - to bottom right). X goes first. ");
+           System.out.println(gameWon);
            Scanner userSelection = new Scanner(System.in);
            int userInput = userSelection.nextInt();
-          if(userInput > 0 && userInput <=9)
+          if(userInput > 0 && userInput <=9 && board[userInput -1] == null )
           {
              // System.out.println("Tester");
               board[userInput - 1] = turn;
-              if(turn == "X")
+              if(turn.equals("X"))
               {
                   turn = "O";
               }
-              else if(turn == "O")
+               else
               {
                   turn = "X";
               }
           }
+             else
+              {
+                  System.out.println("Please enter a valid input or that tile is already filled");
+              }
 
           //check if the game has been won
-           if (board[0] == X || board[0] == O && board[4] == X || board[4] == O && board[8] == X || board[8] == O  ) {
-               gameWon = true;
-           }
-           if (board[0] == X || board[0] == O && board[1] == X || board[1] == O && board[2] == X || board[2] == O  ) {
-               gameWon = true;
-           }
-           if (board[0] == X || board[0] == O && board[3] == X || board[3] == O && board[6] == X || board[6] == O  ) {
-               gameWon = true;
-           }
-           if (board[1] == X || board[1] == O && board[4] == X || board[4] == O && board[7] == X || board[7] == O  ) {
-               gameWon = true;
-           }
-           if (board[2] == X || board[2] == O && board[4] == X || board[4] == O && board[6] == X || board[6] == O  ) {
-               gameWon = true;
-           }
-           if (board[2] == X || board[2] == O && board[5] == X || board[5] == O && board[8] == X || board[8] == O  ) {
-               gameWon = true;
-           }
-           if (board[3] == X || board[3] == O && board[4] == X || board[4] == O && board[5] == X || board[5] == O  ) {
-               gameWon = true;
-           }
-           if (board[6] == X || board[6] == O && board[7] == X || board[7] == O && board[8] == X || board[8] == O  ) {
-               gameWon = true;
-           }
+
+
+           gameWon =  checkIfWon(board, 0,1,2);
+           gameWon =  checkIfWon(board, 0,4,8);
+           gameWon =  checkIfWon(board, 2,4,6);
+           gameWon =  checkIfWon(board, 3,4,5);
+           gameWon =  checkIfWon(board, 0,3,6);
+           gameWon =  checkIfWon(board, 1,4,7);
+           gameWon =  checkIfWon(board, 2,5,8);
+           gameWon =  checkIfWon(board, 6,7,8);
+
+
+
        }
 
 
+    }
+    public static boolean checkIfWon(String[] board ,  int firstValue, int secondValue, int thirdValue)
+    {
+        String X = "X";
+        String O = "O";
+        boolean gameIsWon = false;
+        if (board[firstValue].equals(X) || board[firstValue].equals(O) && board[secondValue].equals(X) || board[secondValue].equals(O) && board[thirdValue].equals(X) || board[thirdValue].equals(O)  ) {
+            gameIsWon = true;
+        }
+        return gameIsWon;
     }
 
 }
