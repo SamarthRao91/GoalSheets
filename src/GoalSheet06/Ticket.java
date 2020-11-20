cbd35528250cd92c8b25de9123c75ed56011f38b
Samarth Rao <raosamarth91@gmail.com>
2020-11-20 14:25:34 -0800

    Worked on ticket

---
+++ src/GoalSheet06/Ticket.java
@@ -0,0 +1,8 @@
+package GoalSheet06;
+
+public class Ticket {
+    public Ticket(int ticketNumber, int ticketPrice)
+    {
+        System.out.println("Ticket Number + "+ ticketNumber + " Price: " + ticketPrice);
+    }
+}
