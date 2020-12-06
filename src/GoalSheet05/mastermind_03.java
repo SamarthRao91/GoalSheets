c33a07ab4aaf3566166170b9c4ae6a62d32f5067
Samarth Rao <raosamarth91@gmail.com>
2020-12-05 23:34:28 -0800

    Made some updates to mastermind

--- src/GoalSheet05/mastermind_03.java
+++ src/GoalSheet05/mastermind_03.java
@@ -1,57 +1,35 @@
 package GoalSheet05;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class mastermind_03 {
     public static void main(String[] args)
-    { //test
-        //declare variables
+    {
         boolean gameWon = false;
-        int blackPegs=0;
-        int whitePegs=0;
-        int counter = 0;
-        int checkForWhite;
+        int nextEmptyIndex=0;
         int[] userGuesses = {0, 0,0,0,0};
+        String[] win = new String[]{"black","black", "black", "black"};
         String[] guessFeedback = new String[4];
         int[] code = {6,6,8,4};
-        int  checkForBlack;
+
         while(!gameWon) {
-            blackPegs = 0;
-            whitePegs = 0;
+
             System.out.println("Please enter your first guess");
             Scanner userInputOne = new Scanner(System.in);
             int firstUserGuess = userInputOne.nextInt( );
             userGuesses[0] = firstUserGuess;
            // checkAnswer(code,guessFeedback,firstUserGuess,1);
-            checkForBlack = checkAnswer(code,guessFeedback,firstUserGuess,1);
-            if(checkForBlack ==1)
-            {
-                counter++;
-                blackPegs++;
-            }
-            if(checkForBlack == 2)
-            {
-                whitePegs++;
-            }
-            System.out.println("So far, you have " + blackPegs + " black pegs and " + whitePegs + " white pegs");
+            guessFeedback = checkAnswer(code,firstUserGuess,1, nextEmptyIndex);
+            System.out.println(Arrays.toString(guessFeedback));
             //enter the second color input
             System.out.println("Please enter your second guess");
             Scanner userInputSecond = new Scanner(System.in);
             int secondUserGuess = userInputSecond.nextInt();
             userGuesses[1] = secondUserGuess;
             //checkAnswer(code,guessFeedback,secondUserGuess,2);
-            checkForBlack = checkAnswer(code,guessFeedback,secondUserGuess,2);
-            if(checkForBlack ==1)
-            {
-                counter++;
-                blackPegs++;
-
-            }
-            if(checkForBlack == 2)
-            {
-                whitePegs++;
-            }
-            System.out.println("So far, you have " + blackPegs + " black pegs and " + whitePegs + " white pegs");
+            guessFeedback = checkAnswer(code,firstUserGuess,2, nextEmptyIndex);
+            System.out.println(Arrays.toString(guessFeedback));
 
             //third colour input
             System.out.println("Please enter your third guess");
@@ -59,18 +37,8 @@ public class mastermind_03 {
             int thirdUserGuess = userInputThird.nextInt();
             userGuesses[2] = thirdUserGuess;
             //checkAnswer(code,guessFeedback,thirdUserGuess,3);
-            checkForBlack = checkAnswer(code,guessFeedback,thirdUserGuess,3);
-            if(checkForBlack ==1)
-            {
-                counter++;
-                blackPegs++;
-
-            }
-            if(checkForBlack == 2)
-            {
-                whitePegs++;
-            }
-            System.out.println("So far, you have " + blackPegs + " black pegs and " + whitePegs + " white pegs");
+            guessFeedback = checkAnswer(code,firstUserGuess,3, nextEmptyIndex);
+            System.out.println(Arrays.toString(guessFeedback));
 
             //fourth user input
             System.out.println("Please enter your fourth guess");
@@ -78,52 +46,35 @@ public class mastermind_03 {
            int fourthUserGuess = userInputFourth.nextInt();
             userGuesses[3] = fourthUserGuess;
             //checkAnswer(code,guessFeedback,fourthUserGuess,4);
-            checkForBlack = checkAnswer(code,guessFeedback,fourthUserGuess,4);
-            if(checkForBlack ==1) {
-                counter++;
-                blackPegs++;
-
-            }
-            if(checkForBlack == 2)
-            {
-                whitePegs++;
-            }
-            System.out.println("So far, you have " + blackPegs + " black pegs and " + whitePegs + " white pegs");
+            guessFeedback = checkAnswer(code,firstUserGuess,4, nextEmptyIndex);
+            System.out.println(Arrays.toString(guessFeedback));
 
             //check if they have won the game
-            if(blackPegs == 4)
+            if(Arrays.equals(guessFeedback, win))
             {
                 gameWon = true;
                 System.out.println("You have guessed the code correctly and won the game");
             }
 
         }
-
-
    }
-   public static int checkAnswer(int[] code, String[] guessFeedback, int userGuess, int guessNumber)
+   public static String[] checkAnswer(int[] code,int userGuess, int guessNumber, int nextEmptyIndex)
    {    int peg = 3;
        int blackCounter = 0;
        int whiteCounter = 0;
+       String[] guessFeedback = new String[4];
        boolean isPeg = false;
-      // int[] valuesChecked = new int[4];
-       //0 is no peg
-
-       //2 is a white peg
-
-       //1 is a black peg
-       // check for the black pegs
        if(userGuess == code[guessNumber -1])
        {
            blackCounter++;
            guessFeedback[0] = "black";
            System.out.println("The guess returned a black peg");
            isPeg = true;
-           peg = 1;
-           return peg;
+           guessFeedback[nextEmptyIndex] = "black";
+           nextEmptyIndex++;
+           System.out.println(nextEmptyIndex);
+           return guessFeedback;
        }
-
-
        //check for the white pegs
        for(int i = code.length - 1; i >=0; i-- )
        {
@@ -133,19 +84,20 @@ public class mastermind_03 {
                guessFeedback[i] = "white";
                System.out.println("The guess returned a white peg");
                isPeg = true;
-               peg = 2;
-               return peg;
+               guessFeedback[nextEmptyIndex] = "white";
+               nextEmptyIndex++;
+               return guessFeedback;
            }
        }
       //checking for no pegs matched
        if(!isPeg)
        {
            System.out.println("Your guess is not in the code");
-           return 3;
+           return guessFeedback;
 
        }
 
-            return peg;
+            return guessFeedback;
                 }
 
 }
