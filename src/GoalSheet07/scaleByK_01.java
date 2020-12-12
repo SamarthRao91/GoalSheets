a7ba41baae36938a4c4fe714a44d954c7e8a38a2
Samarth Rao <raosamarth91@gmail.com>
2020-12-11 17:59:49 -0800

    Finished scaleByK assignment

--- src/GoalSheet07/scaleByK_01.java
+++ src/GoalSheet07/scaleByK_01.java
@@ -2,47 +2,30 @@ package GoalSheet07;
 
 import java.util.*;
 public class scaleByK_01 {
-    static int nextEmptyIndex =0;
+    //static int nextEmptyIndex =0;
     public static void main (String[] args)
     {
-
-        ArrayList<Integer> toBeScaled = new ArrayList<>(Arrays.asList(4,1,5,3));
+        ArrayList<Integer> toBeScaled = new ArrayList<>(Arrays.asList(4,1,5,3, -3, 0, -4, 6, -6));
 
         System.out.println(scalesByK(toBeScaled));
-        System.out.println("Test");
-
     }
 
     public static ArrayList<Integer> scalesByK(ArrayList<Integer> numbers)
     {   int value;
+        ArrayList < Integer > returnAL = new ArrayList<Integer>();
+
         for(int i =0; i < numbers.size(); i++) {
-            //remove the 0's and the negative values
-            if (numbers.get(i) == 0 || numbers.get(i) < 0) {
+            if (numbers.get(i) <= 0)
                 numbers.remove(i);
-            }
         }
-
-        for(int i =0; i < numbers.size(); i++)
+        for(int i =0; i <  numbers.size(); i++)
         {
-
-            //make sure that the value is not null
-            if(numbers.get(i) != null)
+            for(int k =0; k < numbers.get(i); k++)
             {
-                value = numbers.get(i);
-
-                //looping value times and adding (value) at the next EmptyIndex
-                for(int a =0; a < value; a++)
-                {
-
-                    numbers.add(nextEmptyIndex, value);
-                    nextEmptyIndex++;
-                }
-
+                returnAL.add(numbers.get(i));
             }
-
-
         }
-        return numbers;
+        return returnAL;
     }
 
 }
