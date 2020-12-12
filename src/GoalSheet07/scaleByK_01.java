9e56def2ce5470df25b01e8f9dcb6ae1de42d570
Samarth Rao <raosamarth91@gmail.com>
2020-12-11 17:23:49 -0800

    Made some changes to scaleByK_01.java

--- src/GoalSheet07/scaleByK_01.java
+++ src/GoalSheet07/scaleByK_01.java
@@ -1,30 +1,28 @@
 package GoalSheet07;
 
 import java.util.*;
-public class scaleByK {
+public class scaleByK_01 {
     static int nextEmptyIndex =0;
     public static void main (String[] args)
     {
-      ArrayList<Integer> toBeScaled = new ArrayList<Integer>();
 
-        toBeScaled.set(0, 2);
-        toBeScaled.set(1, 3);
-        toBeScaled.set(2, 3);
-        toBeScaled.set(3, 1);
+        ArrayList<Integer> toBeScaled = new ArrayList<>(Arrays.asList(4,1,5,3));
+
         System.out.println(scalesByK(toBeScaled));
+        System.out.println("Test");
 
     }
 
     public static ArrayList<Integer> scalesByK(ArrayList<Integer> numbers)
-    {   int value = 0;
-        for(int i =0; i <= numbers.size(); i++) {
+    {   int value;
+        for(int i =0; i < numbers.size(); i++) {
             //remove the 0's and the negative values
             if (numbers.get(i) == 0 || numbers.get(i) < 0) {
                 numbers.remove(i);
             }
         }
 
-        for(int i =0; i <= numbers.size(); i++)
+        for(int i =0; i < numbers.size(); i++)
         {
 
             //make sure that the value is not null
@@ -36,7 +34,7 @@ public class scaleByK {
                 for(int a =0; a < value; a++)
                 {
 
-                    numbers.set(nextEmptyIndex, value);
+                    numbers.add(nextEmptyIndex, value);
                     nextEmptyIndex++;
                 }
 
