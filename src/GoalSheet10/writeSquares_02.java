e092775a274b8446963a37139d39c155fb26fb20
Samarth Rao <raosamarth91@gmail.com>
2021-02-17 17:19:37 -0800

    Update writeSquares_02.java

--- src/GoalSheet10/writeSquares_02.java
+++ src/GoalSheet10/writeSquares_02.java
@@ -1,17 +1,32 @@
-class Main {
-  public static void main(String[] args) {
-    
-    System.out.println(writeSquares(10));
-  }
-  public static int  writeSquares(int n)
-  {
-    //first n squares, odd in descending and even in acsending
-      if(n<=1)
-      {
-        return 0;
-      }
-      
-      return writeSquares( ((n-1)*(n-1)) + (2*n) -1 )? ;
-        
-  }
-}
+package GoalSheet10;
+
+import java.util.*;
+class writeSquares_02 {
+    public static void main(String[] args) {
+
+        System.out.println(writeSquares(10));
+    }
+    public static int  writeSquares(int n)
+    {
+        //first n squares, odd in descending and even in acsending
+        int value =0;
+        if(n < 2)
+        {
+            return n;
+        }
+        //System.out.print(n * n + " ");
+        value = n*n;
+        if((value % 2) == 0)
+        {
+            //arrange even squares here
+        }
+        else
+        {
+            //arrange odd squares here
+        }
+        return writeSquares(n-1);
+
+    }
+
+
+} 
