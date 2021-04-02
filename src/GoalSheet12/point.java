8ff6f142d4b2b9c233cde11f38d40a80556082fb
Samarth Rao <raosamarth91@gmail.com>
2021-04-02 14:41:24 -0700

    Point Assignment

---
+++ src/GoalSheet12/point.java
@@ -0,0 +1,23 @@
+package GoalSheet12;
+
+import java.lang.Math;
+
+public class point implements Comparable<point> {
+    private float x;
+    private float y;
+
+    public point(float x, float y)
+    {
+      this.x = x;
+      this.y = y;
+    }
+    public int compareTo(point o)
+    {
+        final double distance = Math.sqrt((x - 0) +(y -0));
+        final double otherDistance = Math.sqrt((o.x - 0) +(o.y -0));
+
+
+        return (int) (distance - otherDistance);
+    }
+}
+
