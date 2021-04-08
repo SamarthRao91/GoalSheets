1830a2b3c2231a44a9151c133c0d713af7330f85
Samarth Rao <raosamarth91@gmail.com>
2021-04-07 19:31:19 -0700

    Finished processData assignment
    -Changed to read from a file and create a new student in students array list
    
    -Changed ID to a double from an int
    
    -Added toString() method to print arrayList of objects
    
    -Changed some comparators to sort in the correct order

--- src/GoalSheet12/student.java
+++ src/GoalSheet12/student.java
@@ -9,9 +9,10 @@ import java.util.Scanner;
 
 class student {
     String lastName, firstName, letterGrade;
-    int ID, gradePercentage;
+    int ID;
+    double gradePercentage;
 
-    public student(String firstName, String lastName, String letterGrade, int ID, int gradePercentage) {
+    public student(String lastName, String firstName, String letterGrade, int ID, double gradePercentage) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.gradePercentage = gradePercentage;
@@ -19,6 +20,10 @@ class student {
         this.ID = ID;
 
     }
+    public String toString() {
+        return "Student: " + lastName + ", " + firstName  + ", " + ID + ", " + gradePercentage +  ", " + letterGrade + "\n";
+    }
+
 }
 
      class Sortbylastname implements Comparator<student> {
@@ -35,13 +40,13 @@ class student {
 
      class SortbyID implements Comparator<student> {
          public int compare(student o1, student o2) {
-             return o1.ID - o2.ID;
+             return o2.ID - o1.ID;
          }
      }
 
      class Sortbypercentage implements Comparator<student> {
          public int compare(student o1, student o2) {
-             return o1.gradePercentage - o2.gradePercentage;
+             return (int)(o2.gradePercentage - o1.gradePercentage);
          }
      }
 
@@ -52,10 +57,34 @@ class student {
 
 class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        String line;
+        String word;
         ArrayList<student> students = new ArrayList<student>();
         //open the file, get input and close the file
-        String filePath = "C:\\Users\\raosa\\IdeaProjects\\GoalSheets\\src\\GoalSheet12\\StudentFile";
+        String filePath = "C:\\Users\\raosa\\IdeaProjects\\GoalSheets\\src\\GoalSheet12\\studentFile.txt";
         Scanner fileInput = new Scanner(new File(filePath));
+        while(fileInput.hasNext())
+        {
+            String[] words;
+
+            int Id;
+            double gradePercentage;
+
+            line = fileInput.nextLine();
+            words = line.split("\\s+");
+
+            Id = Integer.parseInt(words[2]);
+            gradePercentage = Double.parseDouble(words[3]);
+            //order of data - last name, first na me, ID, percentage, Grade
+            //order of data needed for constructor -  lastName,  firstName,  letterGrade,  ID,  gradePercentage
+            students.add(new student(words[0],words[1],words[4],Id, gradePercentage));
+        }
+
+        Collections.sort(students, new Sortbyfirstname());
+
+        System.out.println(students);
+
+
 
 
 
