01c0d944b92742f6e4e007f03f427a90abea8476
Samarth Rao <raosamarth91@gmail.com>
2021-04-30 11:33:34 -0700

    Did some more work on Levenshtien

--- src/GoalSheet14/LevenshteinDistance.java
+++ src/GoalSheet14/LevenshteinDistance.java
@@ -1,12 +1,12 @@
 package GoalSheet14;
 /*
-* BJP Ch 11 project 1. Find edit (Levenshtein) distance between words.
-*  Read words from a sorted dictionary file. - X
-*  Compute a map from every word to its immediate neighbors, that is, the words that have an edit distance of 1 from it.
-*  You can walk the map to find paths from one word to another, and can use a linked list of words to visit neighbors
-* By the way, that is a little hint.) One last thing to note, the dictionaries have 370,099 words in them.
-*  Use one or both to help you solve the problem
-* */
+ * BJP Ch 11 project 1. Find edit (Levenshtein) distance between words.
+ *  Read words from a sorted dictionary file. - X
+ *  Compute a map from every word to its immediate neighbors, that is, the words that have an edit distance of 1 from it.
+ *  You can walk the map to find paths from one word to another, and can use a linked list of words to visit neighbors
+ * By the way, that is a little hint.) One last thing to note, the dictionaries have 370,099 words in them.
+ *  Use one or both to help you solve the problem
+ * */
 
 import java.io.*;
 import java.util.*;
@@ -15,17 +15,31 @@ public class LevenshteinDistance {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        ArrayList<String> words = new ArrayList<>();
+        ArrayList < String > words = new ArrayList < > ();
+        Map < String, String > immediateNeighbors = new HashMap < String, String > ();
         String filePath = "C:\\Users\\raosa\\IdeaProjects\\GoalSheets\\src\\GoalSheet14\\Dictionary";
         Scanner fileInput = new Scanner(new File(filePath));
-        while(fileInput.hasNext())
-        {
+        while (fileInput.hasNext()) {
             words.add(fileInput.nextLine());
         }
 
-        int distance =0;
-        distance = findDistance("saturday", "sunday", 8,6 );
-        System.out.println(distance);
+        for (int i = 0; i <= words.size() - 1; i++) {
+            String baseWord = words.get(i);
+
+            for (int k = 0; k <= words.size() - 1; k++) {
+                String targetWord = words.get(k);
+                if (baseWord.length() == targetWord.length() || baseWord.length() == targetWord.length() - 1 || baseWord.length() == targetWord.length() + 1) {
+                    int distance = findDistance(baseWord, targetWord, baseWord.length(), targetWord.length());
+                    if (distance == 1) {
+                        //make a map, with key being the baseWord and targetWord being the value.
+                        immediateNeighbors.put(baseWord, targetWord);
+                    }
+                } else {
+                    break;
+                }
+            }
+
+        }
     }
 
     static int min(int a, int b, int c)
@@ -34,42 +48,34 @@ public class LevenshteinDistance {
     }
 
 
-     static int findDistance(String word1, String word2, int word1Length, int word2Length)
-    {
+    static int findDistance(String word1, String word2, int word1Length, int word2Length) {
         word1Length -= 1;
-        word2Length -=1;
-        int i =1;
-        int j =1;
+        word2Length -= 1;
+        int i = 1;
+        int j = 1;
         int substitutionCost = 0;
         //declaring 2d array, all values are already set to 0
         int[][] wordLength = new int[word1.length()][word2.length()];
 
-      for( i =1; i <= word1Length; i++)
-      {
+        for (i = 1; i <= word1Length; i++) {
             wordLength[i][0] = i;
-      }
-        for( j =1; j <= word2Length; j++)
-        {
+        }
+        for (j = 1; j <= word2Length; j++) {
             wordLength[0][j] = j;
         }
 
 
-        for(j = 1; j <= word2Length; j++)
-        {
-            for(i = 1; i <= word1Length; i++)
-            {
-                if( word1.indexOf(i) == word2.indexOf(j))
-                {
-                   substitutionCost = 0;
-                }
-                else
-                {
+        for (j = 1; j <= word2Length; j++) {
+            for (i = 1; i <= word1Length; i++) {
+                if (word1.indexOf(i) == word2.indexOf(j)) {
+                    substitutionCost = 0;
+                } else {
                     substitutionCost = 1;
                 }
 
-                wordLength[i][j] = min(wordLength[i-1] [j] +1, wordLength[i][j-1] + 1 , wordLength[i-1][ j-1] + substitutionCost);
+                wordLength[i][j] = min(wordLength[i - 1][j] + 1, wordLength[i][j - 1] + 1, wordLength[i - 1][j - 1] + substitutionCost);
 
-             }
+            }
         }
         return wordLength[word1Length][word2Length];
 
