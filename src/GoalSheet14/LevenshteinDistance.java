be5c946812d009cef49d3e2b6d4bb3931f7c30f6
Samarth Rao <raosamarth91@gmail.com>
2021-05-27 22:46:49 -0700

    updated levenshtiens

--- src/GoalSheet14/LevenshteinDistance.java
+++ src/GoalSheet14/LevenshteinDistance.java
@@ -8,6 +8,7 @@ package GoalSheet14;
  *  Use one or both to help you solve the problem
  * */
 
+
 import java.io.*;
 import java.util.*;
 
@@ -16,37 +17,100 @@ public class LevenshteinDistance {
     public static void main(String[] args) throws FileNotFoundException {
 
         ArrayList < String > words = new ArrayList < > ();
-        Map < String, String > immediateNeighbors = new HashMap<>();
+        Map < String, ArrayList<String> > immediateNeighbors = new HashMap<>();
+
         String filePath = "C:\\Users\\raosa\\IdeaProjects\\GoalSheets\\src\\GoalSheet14\\Dictionary";
+
+         //read from the file into the arrayList and sort it alphabetically
         Scanner fileInput = new Scanner(new File(filePath));
         while (fileInput.hasNext()) {
             words.add(fileInput.nextLine());
         }
 
         Collections.sort(words);
+
+        //loop through the arrayList , and check if words[i] has a edit distance of 1 with words[k]
        for (int i = 0; i <= words.size() - 1; i++) {
             String baseWord = words.get(i);
-
+           ArrayList<String> dotWords = new ArrayList<>();
             for (int k = 0; k <= words.size() - 1; k++) {
                 String targetWord = words.get(k);
-                if (baseWord.length() == targetWord.length() || baseWord.length() == targetWord.length() - 1 || baseWord.length() == targetWord.length() + 1) {
+
                     int distance = findDistance(baseWord, targetWord);
                     if (distance == 1) {
-                        //make a map, with key being the baseWord and targetWord being the value.
 
-                        immediateNeighbors.put(baseWord, targetWord);
+                        dotWords.add(targetWord);
+                        immediateNeighbors.put(baseWord, dotWords);
                     }
-                } else {
-                    break;
-                }
+
             }
 
         }
-       immediateNeighbors.forEach((key, value) -> System.out.println(key + ":" + value));
-       //System.out.println("test");
+
+        for (Map.Entry<String, ArrayList<String> > entry : immediateNeighbors.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue().toString());
+        }
+
+        //ask the user for two words and walk the map
+        System.out.println("Please input two words from the dictionary");
+        Scanner userInput = new Scanner(System.in);
+        String first = userInput.nextLine();
+        System.out.println();
+        userInput = new Scanner(System.in);
+        String second = userInput.nextLine();
+        System.out.println();
+
+        //walk the map
+        ArrayList<String> mapValues  = new ArrayList<>();
+        ArrayList<String> retVal = new ArrayList<>();
+        boolean pathFound = false;
+        boolean toContinued = false;
+        int l =0;
+        retVal.add(first);
+        mapValues = immediateNeighbors.get(first);
+
+
+
+        while(!pathFound && ( l < mapValues.size()))
+        {
+            System.out.println("ALsize " + mapValues.size() + " l " + l );
+            for (String mapValue : mapValues) {
+                if (mapValue.equals(second)) {
+                    retVal.add(mapValue);
+                    pathFound = true;
+                }
+                else
+                {
+
+                     for (String s : retVal) {
+                        if (mapValues.get(l).equals(s)) {
+                            toContinued = true;
+                            break;
+                        }
+                    }
+                    if(toContinued)
+                    {
+
+                        l++;
+                        continue;
+                    }
+
+
+
+                    mapValues = immediateNeighbors.get(mapValues.get(l));
+                    retVal.add(mapValues.get(l));
+                     l++;
+
+                }
+            }
+        }
+
+        System.out.println();
+        System.out.println(retVal);
 
 
     }
+
 
     static int min(int a, int b, int c)
     {
